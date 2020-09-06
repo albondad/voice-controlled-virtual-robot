7caac034a291c51654bf22a750867ffcb16aa00a
Al Bondad <albondad@gmail.com>
2020-09-05 20:00:42 -0500

    Add updates for robot simulation

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,4 +1,4 @@
-import React, { useRef, useLayoutEffect } from 'react';
+import React, { useState, useRef, useLayoutEffect } from 'react';
 import { Grid } from '@material-ui/core'
 import { 
     Button,
@@ -9,16 +9,14 @@ import {
 import { rem, em } from 'polished';
 
 export const Home = (props) => {
-    const [xOffset, setXOffset] = useState;
-    document.addEventListener('keypress', (event) => {
-        console.log(event.code)
-        if (event.code === 'keyA') {
 
-        }
-    })
+    
 
     const canvasRef = useRef(null);
-    
+    setInterval(() => {
+        update(data)
+        paint(canvasRef.current, canvasRef.current.getContext('2d'), data);
+    }, 100)
     return (
         <Container
             pt={em(64)}
@@ -74,12 +72,74 @@ export const Home = (props) => {
 Home.defaultProps = {
 }
 
+let xOffset = 64;
+
+let data = {
+    friction: 4,
+    xPosition: 32,
+    yPosition: 32,
+    xVelocity: 64,
+    yVelocity: 64,
+}
+
+const update = (lastData) => {
+    let friction = lastData.friction;
+    let xPosition = lastData.xPosition;
+    let yPosition = lastData.yPosition;
+    let xVelocity = lastData.xVelocity;
+    let yVelocity = lastData.yVelocity;
+
+    if (xVelocity > 0) {
+        xVelocity -=  friction;
+        if (xVelocity < 0) {
+            xVelocity = 0;
+        }
+    }
+    if (xVelocity < 0) {
+        xVelocity +=  friction;
+        if (xVelocity > 0) {
+            xVelocity = 0;
+        }
+    }
+    if (yVelocity > 0) {
+        yVelocity -=  friction;
+        if (yVelocity < 0) {
+           yVelocity = 0;
+        }
+    }
+    if (yVelocity < 0) {
+        yVelocity +=  friction;
+        if (yVelocity > 0) {
+            yVelocity = 0;
+         }
+    }
+
+    xPosition += xVelocity;
+    yPosition += yVelocity;
+
+    data = {
+        ...lastData,
+        xPosition,
+        yPosition,
+        xVelocity,
+        yVelocity
+    }
+}
+
 const paint = (canvas, context, data) => {
+    context.clearRect(0, 0, canvas.with, canvas.height);
+    context.beginPath();
+
+    let defaultWidth = canvas.width;
+    canvas.width = 0;
+    canvas.width = defaultWidth;
+
+
     context.fillStyle = "#333333"
     context.fillRect(0, 0, canvas.width, canvas.height)
 
     context.fillStyle = "#eeeeee"
     context.beginPath;
-    context.arc(canvas.width/2, canvas.height/2, 32, 0, 2 * Math.PI,)
+    context.arc(data.xPosition, data.yPosition, 32, 0, 2 * Math.PI,)
     context.fill();
 }
--- src/components/ui/atoms/canvas.js
+++ src/components/ui/atoms/canvas.js
@@ -1,11 +1,20 @@
-import React, { useState, useLayoutEffect} from 'react';
+import React, { useState, useEffect, useLayoutEffect} from 'react';
 
 export const Canvas = React.forwardRef((props, ref) => {
     const [ dpi, setDpi ] = useState(window.devicePixelRatio);
     const [ height, setHeight ] = useState(0);
     const [ width, setWidth ] = useState(0);
 
+    useEffect(() => {
+        // handle canvas and canvas resolution resizing
+        window.addEventListener('resize', () => {
+            setWidth(ref.current.clientWidth)
+            setHeight(ref.current.clientWidth * 0.5625);
+        })
+    });
+
     useLayoutEffect(() => {
+        // handle canvas and canvas resolution sizing
         setWidth(ref.current.clientWidth)
         setHeight(ref.current.clientWidth * 0.5625);
     })
